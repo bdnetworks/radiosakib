21c44a5f95da1943d580564f240b72a977ee185b
bdnetworks <saakib.com@gmail.com>
2025-08-27 14:30:20 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: 'https://github.io',
         port: '',
         pathname: '/**',
       }
