c12c8340707678c6ef7a8e4e223f2fdad2dcb7a8
bdnetworks <saakib.com@gmail.com>
2025-08-27 15:37:28 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'kendua.github.io' },
-      { protocol: 'https', hostname: 'media.istockphoto.com' },
-      { protocol: 'https', hostname: 't3.ftcdn.net' },
-      { protocol: 'https', hostname: 'images.unsplash.com' }
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blogger.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
